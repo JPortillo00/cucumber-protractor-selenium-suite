3e6ac24b47000a4d3a3776e636615b00c913eb23
Jairo Portillo <portillo.jairojr@gmail.com>
2019-08-12 10:26:47 -0500

    Fixed protractor.conf.js generation.

--- bin/init_project.sh
+++ bin/init_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Preparing protractor.conf.js"
-cat protractor.example.js>protractor.conf.js
+cat ./protractor.example.js > ./protractor.conf.js
 echo "Running npm install"
 npm install
 echo "Updating webdriver"
--- src/step_definitions/action.steps.js
+++ src/step_definitions/action.steps.js
@@ -2,13 +2,14 @@ const {browser, by, element, ExpectedConditions} = require('protractor');
 const {Given, When, Then} = require("cucumber");
 
 When(/^I go to "(.*)"$/, async function (url) {
+    if(!url.includes('http'))
+        url = 'https://' + url;
     await browser.get(url);
 });
 
 Then(/^I should be on "(.*)"$/, async function (url) {
-    try {
-        await browser.wait(ExpectedConditions.urlContains(url));
-    } catch (e) {
-        throw Error(url + "was not found in the URL.")
-    }
+    await browser.wait(
+        ExpectedConditions.urlContains(url),
+        5000,
+        url + "was not found in the URL.");
 });
