b873d03f79379378df95dd44a4e8cd1cb723a562
Jairo Portillo <portillo.jairojr@gmail.com>
2019-08-12 17:12:51 -0500

    Initial Genereic Navigation functionallity.

--- src/step_definitions/action.steps.js
+++ src/step_definitions/action.steps.js
@@ -1,15 +1,19 @@
-const {browser, by, element, ExpectedConditions} = require('protractor');
+const {browser, ExpectedConditions} = require('protractor');
 const {Given, When, Then} = require("cucumber");
 
-When(/^I go to "(.*)"$/, async function (url) {
+When(/^I go to "(.*)"$/, async (url) => {
     if(!url.includes('http'))
         url = 'https://' + url;
     await browser.get(url);
 });
 
-Then(/^I should be on "(.*)"$/, async function (url) {
+Then(/^I should be on "(.*)"$/, async (url) => {
     await browser.wait(
         ExpectedConditions.urlContains(url),
         5000,
         url + "was not found in the URL.");
+});
+
+When(/^I click on "(.*)"$/, async (text) => {
+
 });
--- src/support/navigation.js
+++ src/support/navigation.js
@@ -1,8 +1,18 @@
 import {browser, by, element} from "protractor";
 
-// Helper class for any fancy navigation on the browser.
+// Helper class for any fancy navigation on the page.
 class Navigation{
+    tags = ['section' , 'div', 'a', 'img', 'label', 'select', 'input', 'button', 'ul', 'li', 'span'];
+    attributes = ['type', 'value', 'title', 'href','src', 'alt'];
 
+    static getElement(text){
+        let block = element(by.id(text));
+        if(block.isDisplayed()){
+            return block;
+        }
+
+
+    }
 }
 
 module.exports = new Navigation();
