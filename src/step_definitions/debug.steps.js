2293044aca5851afb30e338aa6b4baa631b0b8d2
Jairo Portillo <portillo.jairojr@gmail.com>
2019-08-13 15:37:17 -0500

    Added larger time out.

--- src/step_definitions/debug.steps.js
+++ src/step_definitions/debug.steps.js
@@ -3,8 +3,8 @@ const {Given, When, Then} = require("cucumber");
 const CustomNavigation = require('../support/navigation');
 
 Then(/^I wait (.*) seconds$/, async (seconds) => {
-    console.log("Sleeping for" + seconds * 1000 + " seconds.");
-    browser.sleep(seconds * 1000);
+    console.log("Sleeping for " + seconds * 1000 + " seconds.");
+    await browser.sleep(seconds * 1000);
 });
 
 Given(/^I see the cursor$/, () => {
--- src/support/hooks.js
+++ src/support/hooks.js
@@ -1,5 +1,21 @@
-// Custom tags for scenarios
-const {After, Before, BeforeAll} = require("cucumber");
+const {After, Before, BeforeAll, AfterAll} = require("cucumber");
+const {browser} = require('protractor');
+
+BeforeAll({timeout: 100 * 1000}, async () => {
+    await browser.get(browser.params.baseURL);
+});
+
+After(async function(scenario) {
+    if (scenario.result.status === Status.FAILED) {
+        // screenShot is a base-64 encoded PNG
+        const screenShot = await browser.takeScreenshot();
+        this.attach(screenShot, "image/png");
+    }
+});
+
+AfterAll({timeout: 100 * 1000}, async () => {
+    await browser.quit();
+});
 
 Before({tags: '@skip'}, () => {
     return 'skipped';
